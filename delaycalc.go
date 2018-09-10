e12bcde0a9a66840a126f7d33be7f24027b1fc9d
Andeman <as@saturn.andeman.de>
2018-09-10 20:56:03 +0200

    inital commit

--- delaycalc.go
+++ delaycalc.go
@@ -1,42 +1,26 @@
 package main
-
 import (
 	"fmt"
 	"math"
-	"flag"
 )
 
-const defaultdepth =  11
-const defaultBars = 4
+const maxDepth =  11
+const upperBar = 4
 const defaultBpm = 120
 
-var bpm float64
-var depth int
-var bars int
-
 func main() {
-
-    initFlags()
+	bpm := float64(defaultBpm)
 	quarter := float64(60 / bpm) 
 	whole := float64(4 * quarter)
-	wholeMs := whole * 1000
-
-    printHeader(bpm)
-	printNotes(wholeMs, depth, bars)
+	wholeMs := whole * 1000	
+	printNotes(wholeMs, maxDepth)
 }
 
-func initFlags() {
-    flag.Float64Var(&bpm, "bpm", 120, "Beats per Minute")
-    flag.IntVar(&depth, "depth", 11, "The lowest divider of one note to show.")
-    flag.IntVar(&bars, "bars", 4, "The amount of bars to display")
-    flag.Parse()
-}
+func Round(x, unit float64) float64 { 
+    return math.Round(x/unit) * unit 
+} 
 
-func printHeader(bpm float64) {
-    fmt.Printf("%+7v %10.2f %v\n", "Tempo:", bpm, "BPM")
-}
-
-func printNotes(whole float64, depth int, upperBar int) {
+func printNotes(whole float64, depth int) {
 	subBar := 1
 	stepValue := whole
 	currentBar := upperBar
@@ -52,15 +36,11 @@ func printNotes(whole float64, depth int, upperBar int) {
 			stepValue = whole * float64(currentBar)
 		}
 		
-		barPosition := fmt.Sprintf("%v/%v:",currentBar, subBar)
+		barPointer := fmt.Sprintf("%v/%v:",currentBar, subBar)
 		stepValueMs := Round(stepValue, 0.01)
 		stepValueSec := Round((stepValueMs / 1000), 0.001)
-		currentBar = upperBar - step
-
-		fmt.Printf("%+7v %10.2f ms %10.3f s \n", barPosition, stepValueMs, stepValueSec)
+		fmt.Printf("%+7v %10.2f ms %10.3f s\n", barPointer, stepValueMs, stepValueSec)
+		currentBar = upperBar - step 
 	}
 }
 
-func Round(x, unit float64) float64 {
-    return math.Round(x/unit) * unit
-}
